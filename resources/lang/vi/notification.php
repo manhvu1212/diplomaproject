eb3e88d4ea7c4b38319bce1df86c07b84c611c9d
vu <vunm@tmh-techlab.vn>
2016-03-10 17:35:04 +0700

    begin activation

---
+++ resources/lang/vi/notification.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vu
+ * Date: 3/10/16
+ * Time: 5:23 PM
+ */
+
+return [
+    'success' => 'Thành công',
+    'error' => 'Lỗi',
+    'not_correct' => 'Email hoặc mật khẩu không đúng',
+    'not_active' => 'Tài khoản chưa được kích hoạt',
+
+];
